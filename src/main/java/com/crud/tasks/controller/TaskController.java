235733eec72ef63edc6bb08821b3a2bce3cee875
Paweloz <pawel.przybyla.1992@o2.pl>
2021-06-05 18:52:08 +0200

    Fixing mapping annotation

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -7,27 +7,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/v1/tasks")
+@RequestMapping("/v1/task")
 public class TaskController {
 
-    @RequestMapping(method = RequestMethod.GET, value = "/getTasks")
+    @GetMapping(value = "/getTasks")
     public List<TaskDto> getTasks() {
         return new ArrayList<>();
     }
 
     @GetMapping(value = "/getTask")
     public TaskDto getTask(Long taskId) {
-        return new TaskDto(1L, "test title", "test_content");
+        return new TaskDto(1L,
+                "test title",
+                "test_content");
     }
 
     @DeleteMapping(value = "/deleteTask")
     public void deleteTask(Long taskId) {
 
     }
+
     @PutMapping(value = "/updateTask")
     public TaskDto updateTask(TaskDto taskDto) {
-        return new TaskDto(1L, "Edited test title", "Test content");
+        return new TaskDto(1L,
+                "Edited test title",
+                "Test content");
     }
+
     @PostMapping(value = "/createTask")
     public void createTask(TaskDto taskDto) {
 
