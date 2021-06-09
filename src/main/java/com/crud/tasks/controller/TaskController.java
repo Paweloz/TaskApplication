a1cbefda222ea43189141bcc41f0bf441bf03566
Paweloz <pawel.przybyla.1992@o2.pl>
2021-06-09 17:03:10 +0200

    19.2 Retrive task by id from database

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -1,25 +1,33 @@
 package com.crud.tasks.controller;
 
+import com.crud.tasks.domain.Task;
 import com.crud.tasks.domain.TaskDto;
+import com.crud.tasks.mapper.TaskMapper;
+import com.crud.tasks.service.DbService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/v1/task")
+@RequiredArgsConstructor
 public class TaskController {
+
+    private final DbService service;
+    private final TaskMapper taskMapper;
+
 
     @GetMapping(value = "/getTasks")
     public List<TaskDto> getTasks() {
-        return new ArrayList<>();
+        List<Task> tasksList = service.getAllTasks();
+        return taskMapper.mapToTaskDtoList(tasksList);
     }
 
     @GetMapping(value = "/getTask")
     public TaskDto getTask(Long taskId) {
-        return new TaskDto(1L,
-                "test title",
-                "test_content");
+        Task task = service.getTask(taskId);
+        return taskMapper.mapToTaskDto(task);
     }
 
     @DeleteMapping(value = "/deleteTask")
--- src/main/java/com/crud/tasks/domain/Task.java
+++ src/main/java/com/crud/tasks/domain/Task.java
@@ -1,7 +1,25 @@
 package com.crud.tasks.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Task {
+
+    @Id
+    @NotNull
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name ="name")
     private String title;
+    @Column(name = "description")
     private String content;
 }
