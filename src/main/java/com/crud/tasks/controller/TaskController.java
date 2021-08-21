0b0448984548c65afd226a45899e4174ebb90cfb
Paweloz <pawel.przybyla.1992@o2.pl>
2021-08-21 14:47:50 +0200

    mapping fix

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -20,31 +20,31 @@ public class TaskController {
     private final TaskMapper taskMapper;
 
 
-    @GetMapping(value = "/getTasks")
+    @GetMapping(value = "getTasks")
     public List<TaskDto> getTasks() {
         List<Task> tasksList = service.getAllTasks();
         return taskMapper.mapToTaskDtoList(tasksList);
     }
 
-    @GetMapping(value = "/getTask")
+    @GetMapping(value = "getTask")
     public TaskDto getTask(@RequestParam Long taskId) throws TaskNotFoundException {
         Task task = service.getTask(taskId).orElseThrow(TaskNotFoundException::new);
         return taskMapper.mapToTaskDto(task);
     }
 
-    @DeleteMapping(value = "/deleteTask")
+    @DeleteMapping(value = "deleteTask")
     public void deleteTask(@RequestParam Long taskId) {
         service.deleteTask(taskId);
     }
 
-    @PutMapping(value = "/updateTask")
+    @PutMapping(value = "updateTask")
     public TaskDto updateTask(@RequestBody TaskDto taskDto) {
         Task task = taskMapper.mapToTask(taskDto);
         Task savedTask = service.saveTask(task);
         return taskMapper.mapToTaskDto(savedTask);
     }
 
-    @PostMapping(value = "/createTask", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "createTask", consumes = MediaType.APPLICATION_JSON_VALUE)
     public void createTask(@RequestBody TaskDto taskDto) {
         Task task = taskMapper.mapToTask(taskDto);
         service.saveTask(task);
