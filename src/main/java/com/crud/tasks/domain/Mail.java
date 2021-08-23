0bacbcbc770c61ea718568fb7311f2cda09ff84f
Paweloz <pawel.przybyla.1992@o2.pl>
2021-08-23 13:42:25 +0200

    23.3 additional email receiver - CC

---
+++ src/main/java/com/crud/tasks/domain/Mail.java
@@ -0,0 +1,13 @@
+package com.crud.tasks.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class Mail {
+    private final String receiverEmail;
+    private final String subject;
+    private final String message;
+    private final String toCc;
+}
---
+++ src/test/java/com/crud/tasks/service/SimpleEmailServiceTest.java
@@ -0,0 +1,44 @@
+package com.crud.tasks.service;
+
+import com.crud.tasks.domain.Mail;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class SimpleEmailServiceTest {
+    @InjectMocks
+    private SimpleEmailService simpleEmailService;
+    @Mock
+    private JavaMailSender javaMailSender;
+
+    @Test
+    void shouldSendMail() {
+        //Given
+        Mail mail = Mail.builder()
+                .receiverEmail("test@test.com")
+                .subject("Test")
+                .message("Test")
+                .toCc("test2@test2.com")
+                .build();
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(mail.getReceiverEmail());
+        mailMessage.setSubject(mail.getSubject());
+        mailMessage.setText(mail.getMessage());
+        mailMessage.setCc(mail.getToCc());
+
+        //When
+        simpleEmailService.send(mail);
+
+        //Then
+        verify(javaMailSender, times(1)).send(mailMessage);
+    }
+}
