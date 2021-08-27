a8861f53cc399522c06d870513c8e10ff5a32b9d
Paweloz <pawel.przybyla.1992@o2.pl>
2021-08-27 18:16:53 +0200

    29.1 Testing TrelloMapper

--- src/main/java/com/crud/tasks/domain/TrelloBoard.java
+++ src/main/java/com/crud/tasks/domain/TrelloBoard.java
@@ -11,4 +11,9 @@ public class TrelloBoard {
     private String id;
     private String name;
     private List<TrelloList> lists;
+
+    public TrelloBoard(String name, List<TrelloList> lists) {
+        this.name = name;
+        this.lists = lists;
+    }
 }
