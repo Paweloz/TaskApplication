e94d46f55e6ec3661caedf9bddc19485097c4872
Paweloz <pawel.przybyla.1992@o2.pl>
2021-09-12 16:31:03 +0200

    if and loop thymeleaf example

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collector;
 
 @Service
@@ -23,13 +25,21 @@ public class MailCreatorService {
 
     public String buildTrelloCardEmail(String message, String goodbye_message) {
         Context context = new Context();
+        List<String> functionality = new ArrayList<>();
+        functionality.add("You can manage your tasks");
+        functionality.add("Provides connection with Trello Account");
+        functionality.add("Application allows sending tasks to Trello");
+
         context.setVariable("message", message);
         context.setVariable("preview_message", message.substring(0,20));
         context.setVariable("tasks_url", "http://localhost:8888/tasks_frontend");
         context.setVariable("button", "Visit website");
-        context.setVariable("admin_name", adminConfig.getAdminName());
+        context.setVariable("admin_name", adminConfig);
         context.setVariable("company_details", adminConfig.getCompanyDetails());
         context.setVariable("goodbye_message", goodbye_message);
+        context.setVariable("show_button", false);
+        context.setVariable("is_friend", true);
+        context.setVariable("application_functionality", functionality);
         return templateEngine.process("mail/created-trello-card-mail", context);
     }
 
